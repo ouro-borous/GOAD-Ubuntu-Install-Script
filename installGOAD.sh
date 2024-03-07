420c624e22e74bc10a165e904267ea792b2822f1
Marko Morrison <88122773+ouro-borous@users.noreply.github.com>
2024-03-07 01:28:35 -0800

    Update installGOAD.sh

--- installGOAD.sh
+++ installGOAD.sh
@@ -1,4 +1,3 @@
-sudo su
 apt update ; apt full-upgrade -y
 
 #VIRTUALBOX INSTALL
@@ -6,32 +5,32 @@ apt install virtualbox
 
 #DOCKER INSTALL
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt update
+sudo apt install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 
 #VAGRANT INSTALL
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant
+sudo apt update && sudo apt install vagrant -y
 
 #WINRM/RUBY INSTALL
-apt install ruby ruby-dev
+apt install ruby ruby-dev -y
 gem install winrm winrm-fs winrm-elevated
 
 #GIT INSTALL
-apt install git
+apt install git -y
 
 #GOAD INSTALL
 git clone https://github.com/Orange-Cyberdefense/GOAD.git
